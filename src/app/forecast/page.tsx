70bcaafaa038c0ea392d2ab11abc2832b3a32fb7
Dmitrii Biblev <biblev1608@gmail.com>
2025-03-18 14:56:32 +0100

    created base layout for forecast page

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -1,7 +1,25 @@
+import s from './page.module.scss';
+
 export default function Forecast() {
   return (
-    <div className="container">
-      Forecast
+    <div className="container py-4 d-flex flex-column gap-3">
+      <h1>Subotica, RS</h1>
+
+      <div className={ s.list }>
+        {
+          ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'San', 'Mon'].map((item) => (
+            <div key={ item } className={ s.day }>
+              <div>
+                { item }
+              </div>
+
+              <div>
+                3&deg;C
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
